ac3c468148faa9e29a5d0ea433a495f64a304746
Hasan <aktashasan1130@gmail.com>
2022-02-16 09:42:13 +0300

    UserServiceTest is completed

--- src/main/java/com/example/cardentity/user/repository/UserRepository.java
+++ src/main/java/com/example/cardentity/user/repository/UserRepository.java
@@ -4,10 +4,13 @@ import com.example.cardentity.user.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
     User findByUsernameAndPassword(String username, String password);
 
     User findByUsername(String username);
+
 
 }
--- src/test/java/com/example/cardentity/user/service/UserServiceTest.java
+++ src/test/java/com/example/cardentity/user/service/UserServiceTest.java
@@ -1,4 +1,96 @@
 package com.example.cardentity.user.service;
 
-public class UserServiceTest {
+import com.example.cardentity.builder.user.UserBuilder;
+import com.example.cardentity.user.model.User;
+import com.example.cardentity.user.model.UserDTO;
+import com.example.cardentity.user.model.UserMapperImpl;
+import com.example.cardentity.user.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest
+@AutoConfigureDataMongo
+class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void addUser() {
+        UserDTO userDTO = new UserBuilder()
+                .buildSomeDummy()
+                .withFirstName("Hasan")
+                .build();
+        UserDTO savedUser = userService.addUser(userDTO);
+        Assertions.assertNotNull(savedUser.getId());
+    }
+
+    @Test
+    void findUserById() {
+        userRepository.deleteAll();
+        UserDTO userDTO = new UserBuilder()
+                .buildSomeDummy()
+                .withFirstName("Hasan")
+                .build();
+        UserDTO savedUser = userService.addUser(userDTO);
+        UserDTO findUser = userService.findUserById(savedUser.getId());
+        Assertions.assertNotNull(findUser);
+    }
+
+    @Test
+    void deleteUserById() {
+        UserDTO userDTO = new UserBuilder()
+                .buildSomeDummy()
+                .withFirstName("Hasan")
+                .build();
+        UserDTO savedUser = userService.addUser(userDTO);
+        boolean deletedUser = userService.deleteUserById(savedUser.getId());
+        Assertions.assertEquals(deletedUser,true);
+
+    }
+
+    @Test
+    void findUserByUsernameAndPassword() {
+        userRepository.deleteAll();
+        UserDTO userDTO = new UserBuilder()
+                .buildSomeDummy()
+                .withUsername("asdfs")
+                .withPassword("asdfsad")
+                .build();
+        UserDTO savedUser = userService.addUser(userDTO);
+        UserDTO findUser = userService.findUserByUsernameAndPassword(userDTO.getUsername(),userDTO.getPassword());
+        Assertions.assertEquals(savedUser,findUser);
+    }
+
+    @Test
+    void findByUsername() {
+        userRepository.deleteAll();
+        UserDTO userDTO = new UserBuilder()
+                .buildSomeDummy()
+                .withUsername("Hasanaktas")
+                .build();
+        UserDTO savedUser = userService.addUser(userDTO);
+        User findUsername = userService.findByUsername(savedUser.getUsername());
+        Assertions.assertEquals(savedUser,UserMapperImpl.toDTO(findUsername));
+
+    }
+    @Test
+    void findAllUsers(){
+        userRepository.deleteAll();
+        UserDTO userDTO = new UserBuilder()
+                .buildSomeDummy()
+                .build();
+        userService.addUser(userDTO);
+        List<UserDTO> userDTOList = userService.findAllUsers();
+        Assertions.assertEquals(1,userDTOList.size());
+    }
 }
