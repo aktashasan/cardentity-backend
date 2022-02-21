a6ecbdc3f376189476b5040aa0b30fc1582963b5
Hasan <aktashasan1130@gmail.com>
2022-02-21 15:25:38 +0300

    resource completed

--- src/main/java/com/example/cardentity/record/controller/RecordResource.java
+++ src/main/java/com/example/cardentity/record/controller/RecordResource.java
@@ -1,4 +1,48 @@
 package com.example.cardentity.record.controller;
 
+import com.example.cardentity.record.model.RecordDTO;
+import com.example.cardentity.record.service.RecordService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("/app")
+@AllArgsConstructor
 public class RecordResource {
+
+    private RecordService recordService;
+
+    @PostMapping("/record/save")
+    public ResponseEntity<RecordDTO> addRecord(@RequestBody RecordDTO recordDTO){
+        return ResponseEntity.ok(recordService.addRecord(recordDTO));
+    }
+
+    @GetMapping("/record/delete/{id}")
+    public ResponseEntity<Boolean> deleteRecordById(@PathVariable String id){
+        return ResponseEntity.ok(recordService.deleteRecordById(id));
+    }
+
+    @GetMapping("/record/get/id/{id}")
+    public ResponseEntity<RecordDTO> findRecordById(@PathVariable String id){
+        RecordDTO recordDTO = recordService.findRecordById(id);
+        if (recordDTO == null) {
+            return  ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(recordDTO);
+    }
+
+    @GetMapping("/record/get/operation/{operationType}")
+    public ResponseEntity<List<RecordDTO>> findRecordByOperationType(@PathVariable String operationType){
+        return ResponseEntity.ok(recordService.findRecordByOperationType(operationType));
+    }
+
+    @GetMapping("/record/get/time/{from}/{to}")
+    public ResponseEntity<List<RecordDTO>> findByTypeBetween(@PathVariable Date from,
+                                                             @PathVariable Date to){
+        return ResponseEntity.ok(recordService.findByTimeBetween(from,to));
+    }
 }
--- src/main/java/com/example/cardentity/user/controller/UserResource.java
+++ src/main/java/com/example/cardentity/user/controller/UserResource.java
@@ -1,4 +1,51 @@
 package com.example.cardentity.user.controller;
 
+import com.example.cardentity.user.model.User;
+import com.example.cardentity.user.model.UserDTO;
+import com.example.cardentity.user.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/app")
+@AllArgsConstructor
 public class UserResource {
+    private UserService userService;
+
+    @PostMapping("/user/save")
+    public ResponseEntity<UserDTO> addUser(@RequestBody UserDTO userDTO){
+        return ResponseEntity.ok(userService.addUser(userDTO));
+    }
+
+    @GetMapping("/user/delete/{id}")
+    public ResponseEntity<Boolean> deleteUserById(@PathVariable String id){
+        return ResponseEntity.ok(userService.deleteUserById(id));
+    }
+
+    @GetMapping("/user/get/id/{id}")
+    public ResponseEntity<UserDTO> findUserById(@PathVariable String id){
+        UserDTO userDTO =userService.findUserById(id);
+        if (userDTO == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(userDTO);
+    }
+
+    @GetMapping("/user/get/usernameAndPassword/{username}/{password}")
+    public ResponseEntity<UserDTO> findUserByUsernameAndPassword(@PathVariable String username,
+                                                                 @PathVariable String password){
+        return ResponseEntity.ok(userService.findUserByUsernameAndPassword(username,password));
+    }
+
+    @GetMapping("/user/get/username/{username}")
+    public ResponseEntity<User> findByUsername(@PathVariable String username){
+        return ResponseEntity.ok(userService.findByUsername(username));
+    }
+    @GetMapping("/users/get")
+    public ResponseEntity<List<UserDTO>> findAllUsers(){
+        return ResponseEntity.ok(userService.findAllUsers());
+    }
 }
